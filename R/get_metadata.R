5b9c9e9463728a8523f5fc4b838848d816cbd485
Zach McAdams <zlmcadams@gmail.com>
2026-03-09 21:13:16 -0500

    Updating getEGG().

--- R/get_metadata.R
+++ R/get_metadata.R
@@ -1,25 +1,51 @@
-#' Pull EGG Data Files
+#' getEGG
 #'
-#' @param version Version of EGG database; default = 1.0.0
+#' @param version Version of EGG database to be downloaded
+#' @param data_type Type of data to be downloaded (metadata, table, or taxonomic assignments)
 #'
-#' @returns
+#' @returns A R object of the requested data type from the EGG database
 #' @export
 #'
 #' @examples
+#'metadata = getEGG(version = "1.0.0", data_type = "metadata")
 #'
+#'metadata = getEGG(version = "1.0.0", data_type = "table")
+#'
+#'metadata = getEGG(version = "1.0.0", data_type = "taxonomy")
+
 getEGG = function(version = "1.0.0",
                   data_type = c('metadata', 'table', 'taxonomy')){
-  zenodo_id = .pkg_env$database_versions[[version]]
-  data_type = .pkg_env$data_type[[data_type]]
+  tryCatch({
+    zenodo_id = match.arg(
+      .pkg_env$database_versions[[version]]
+    )
+  }, error = function(e){
+    stop("Version number not found. Run get_EGG_version() for avaible versions.")
+  })
+  tryCatch({
+    data_type = match.arg(
+      .pkg_env$data_type[[data_type]]
+    )
+  }, error = function(e){
+    stop('Data type unavailable. Please select from c("metadata", "table", "taxonomy"')
+  })
+
   url = paste0("https://zenodo.org/records/",
                zenodo_id,
                "/files/",
                data_type,
                ".Rds")
+
+  if(!file.exists(url)){
+    stop(paste("File does not exist at:", url))
+  }
+
   tmp = "tmp/data.Rds"
   download.file(url, tmp)
   data = readRDS(tmp)
+
   file.remove(tmp)
+
   return(data)
 }
 
